8f308418ad99734f6ff0b52db036593f929be6db
adityabhartib <adityabharti.ab@gmail.com>
2022-10-12 11:59:46 +0530

    UC2 Equality of Two line Added

--- src/com/bridgelabz/LineComparisonProgram.java
+++ src/com/bridgelabz/LineComparisonProgram.java
@@ -15,7 +15,24 @@ public class LineComparisonProgram {
         System.out.print("Enter y2 of Second Point: ");
         int y2 = scanner.nextInt();
 
-        double lenghtOfLine = Math.sqrt(Math.pow(x2-x1, 2) + Math.pow(y2-y1,2));    // calculate the length of line
-        System.out.printf("Length of Line is : %.2f %n", lenghtOfLine);
+        System.out.print("Enter x1 of Third Point: ");
+        int p1 = scanner.nextInt();
+        System.out.print("Enter y1 of Third Point: ");
+        int q1 = scanner.nextInt();
+        System.out.print("Enter x2 of Forth Point: ");
+        int p2 = scanner.nextInt();
+        System.out.print("Enter y2 of Forth Point: ");
+        int q2 = scanner.nextInt();
+
+        Double lengthOfLine1 = Math.sqrt(Math.pow(x2-x1, 2) + Math.pow(y2-y1,2));
+        double lengthOfLine2 = Math.sqrt(Math.pow(p2-p1, 2) + Math.pow(q2-q1, 2));
+
+        double result = lengthOfLine1.compareTo(lengthOfLine2);
+        if(result > 0)
+            System.out.println("Length of First end points is Greater than Length of Second end Points");
+        else if(result < 0)
+            System.out.println("Length of First end points is Less than Length of Second end Points");
+        else
+            System.out.println("Both lines are Equal");
     }
 }
